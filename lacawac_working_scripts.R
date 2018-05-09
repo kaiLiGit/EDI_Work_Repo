9760acaed79ce84cf027049050b988d11344d0e3
kaiLiGit <mygitacc50@gmail.com>
2018-05-09 12:18:06 -0400

    1 - version of lacawac gile using EMLAssemblyLine

---
+++ lacawac_working_scripts.R
@@ -0,0 +1,13 @@
+make_eml(path = "C:/Users/natev/Desktop/EDI_workfiles/Lacawac_Giles_Directory/Lacawac_Giles_Directory",
+         dataset.title = "LacGilTempDO from Penn:1988_2016", 
+         data.files = c("2018_03_09_LacGilTempDO_1988_2016"),
+         data.files.description = c("Lake Temperature Data && Dissolved Oxygen Concentration"),
+         data.files.quote.character = c("\""),
+         temporal.coverage = c("1988-01-31", "2016-01-01"),
+         geographic.description = "United States, Pennsylvania", 
+         geographic.coordinates = c("41.381964",	"-75.292494","41.376157",	"-75.092916"),
+         maintenance.description = "completed", 
+         user.id = "bmette", 
+         package.id = "edi.201.1")
+
+
